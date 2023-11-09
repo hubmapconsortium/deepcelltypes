7b1748f0a460024fb13a2d7334b720282f6a143e
xuefei-wang <wang.xuefei.wang@gmail.com>
2023-11-09 14:59:58 -0800

    Update file by moving funcs and configs to deepcelltype_kit

--- main.py
+++ main.py
@@ -2,89 +2,22 @@ import numpy as np
 import scipy as sp
 import tifffile as tff
 from ome_types import from_tiff
-from skimage.exposure import rescale_intensity, equalize_adapthist
 from skimage.measure import regionprops
 from skimage.transform import resize
 from tensorflow.keras.models import load_model
 import click
 import json
 
-MAX_NUM_CHANNELS = 52
+from deepcelltypes_kit.config import DCTConfig
+from deepcelltypes_kit.image_funcs import (
+    histogram_normalization,
+    pad_cell,
+    get_crop_box,
+    get_neighbor_masks,
+    select_raw_combine_mask,
+)
 
-
-def histogram_normalization(image, kernel_size=None):
-    """
-    Pre-process images using Contrast Limited Adaptive
-    Histogram Equalization (CLAHE).
-    If one of the inputs is a constant-value array, it will
-    be normalized as an array of all zeros of the same shape.
-    Args:
-        image (numpy.array): numpy array of phase image data with shape
-            (H, W, C). Note there is no batch index here.
-        kernel_size (integer): Size of kernel for CLAHE,
-            defaults to 1/8 of image size.
-    Returns:
-        numpy.array: Pre-processed image data with dtype float32.
-    """
-    image = image.astype("float32")
-    assert len(image.shape) == 3
-
-    for channel in range(image.shape[-1]):
-        X = image[..., channel]
-        sample_value = X[(0,) * X.ndim]
-        if (X == sample_value).all():
-            image[..., channel] = np.zeros_like(X)
-            continue
-
-        X = rescale_intensity(X, out_range=(0.0, 1.0))
-        X = equalize_adapthist(X, kernel_size=kernel_size)
-        image[..., channel] = X
-    return image
-
-
-def pad_cell(X: np.ndarray, y: np.ndarray, crop_size: int):
-    delta = crop_size // 2
-    X = np.pad(X, ((delta, delta), (delta, delta), (0, 0)))
-    y = np.pad(y, ((delta, delta), (delta, delta)))
-    return X, y
-
-
-def get_crop_box(centroid, delta):
-    minr = int(centroid[0]) - delta
-    maxr = int(centroid[0]) + delta
-    minc = int(centroid[1]) - delta
-    maxc = int(centroid[1]) + delta
-    return np.array([minr, minc, maxr, maxc])
-
-
-def get_neighbor_masks(mask, cbox, cell_idx):
-    """Returns binary masks of a cell and its neighbors. This function expects padding around
-    the edges, and will throw an error if you hit a wrap around."""
-    minr, minc, maxr, maxc = cbox
-    assert np.issubdtype(mask.dtype, np.integer) and isinstance(cell_idx, int)
-
-    cell_view = mask[minr:maxr, minc:maxc]
-    binmask_cell = (cell_view == cell_idx).astype(np.int32)
-
-    binmask_neighbors = (cell_view != cell_idx).astype(np.int32) * (
-        cell_view != 0
-    ).astype(np.int32)
-    return binmask_cell, binmask_neighbors
-
-
-def select_raw_combine_mask(raw, mask, channel_mask):
-    new_raw = raw[channel_mask]  # (C_new, H, W)
-    raw_aug_mask = np.concatenate(
-        [
-            np.expand_dims(new_raw, axis=-1),  # (C_new, H, W, 1)
-            np.tile(
-                np.expand_dims(mask, axis=0), (len(new_raw), 1, 1, 1)
-            ),  # (C_new, H, W, 2)
-        ],
-        axis=-1,
-    )  # (C_new, H, W, 3)
-
-    return raw_aug_mask
+dct_config = DCTConfig()
 
 
 @click.command()
@@ -119,62 +52,12 @@ def pipeline_main(data_dir, image_fname, segmask):
 
     # Get model channels and cell types
     model_dir = Path("../model/saved_model")
-    config_path = Path("../model/config.yaml")
     channel_mapping_path = Path("../model/channel_mapping.yaml")
-    with open(config_path, "r") as fh:
-        model_config = yaml.load(fh, yaml.Loader)
-    master_channel_lst = model_config["channels"]
 
     with open(channel_mapping_path, "r") as fh:
         channel_mapping = yaml.load(fh, yaml.Loader)
 
-    mapper_dict = {
-        "Acinar": 1,
-        "Astrocyte": 2,
-        "Bcell": 3,
-        "BloodVesselEndothelial": 4,
-        "CD4T": 5,
-        "CD8T": 6,
-        "Dendritic": 7,
-        "EVT": 8,
-        "Endocrine": 9,
-        "Endothelial": 10,
-        "Enterocyte": 11,
-        "Epithelial": 12,
-        "Fibroblast": 13,
-        "GiantCell": 14,
-        "Goblet": 15,
-        "ICC": 16,
-        "Immune": 17,
-        "LymphaticEndothelial": 18,
-        "Lymphocyte": 19,
-        "M1Macrophage": 20,
-        "M2Macrophage": 21,
-        "Macrophage": 22,
-        "Mast": 23,
-        "Monocyte": 24,
-        "Muscle": 25,
-        "Myeloid": 26,
-        "Myoepithelial": 27,
-        "Myofibroblast": 28,
-        "NK": 29,
-        "NKT": 30,
-        "Nerve": 31,
-        "Neuroendocrine": 32,
-        "Neuron": 33,
-        "Neutrophil": 34,
-        "Paneth": 35,
-        "Plasma": 36,
-        "Secretory": 37,
-        "SmoothMuscle": 38,
-        "Stromal": 39,
-        "Tcell": 40,
-        "TransitAmplifying": 41,
-        "Treg": 42,
-        "Tumor": 43,
-        "Unknown": 44,
-    }
-    mapper_dict_reversed = {v: k for k, v in mapper_dict.items()}
+    mapper_dict_reversed = {v: k for k, v in dct_config.mapper_dict.items()}
 
     # Store info on channel mappings for post-evaluation
     marker_info = {}
@@ -185,12 +68,12 @@ def pipeline_main(data_dir, image_fname, segmask):
     img_metadata = from_tiff(data_file)
     ch_names = [ch.name for ch in img_metadata.images[0].pixels.channels]
     marker_info["img_marker_panel"] = ch_names
-    marker_info["model_marker_panel"] = master_channel_lst
+    marker_info["model_marker_panel"] = dct_config.master_channels
     channel_lst = []
     channel_mask = []
     for idx, ch in enumerate(ch_names):
         key = channel_mapping[ch]
-        if key in master_channel_lst:
+        if key in dct_config.master_channels:
             channel_lst.append(key)
             channel_mask.append(True)
         else:
@@ -206,11 +89,8 @@ def pipeline_main(data_dir, image_fname, segmask):
 
     class_X = multiplex_img.astype(np.float32)
     print("class_X.shape", class_X.shape)
-    kernel_size = 128
-    crop_size = 64
-    rs = 32
     # check master list against channel_lst
-    assert not set(channel_lst) - set(master_channel_lst)
+    assert not set(channel_lst) - set(dct_config.master_channels)
     # assert len(channel_lst) == class_X.shape[-1]
 
     ctm = load_model(model_dir, compile=False)
@@ -220,12 +100,12 @@ def pipeline_main(data_dir, image_fname, segmask):
     pred = tff.imread(mask_path)[0, 0, ...]
     assert pred.shape == class_X.shape[:-1]
 
-    X = histogram_normalization(class_X, kernel_size=kernel_size)
+    X = histogram_normalization(class_X, kernel_size=dct_config.HIST_NORM_KERNEL_SIZE)
 
     # this is set up for one batch at a time
     y = pred
     # B, Y, X, C
-    X, y = pad_cell(X, y, crop_size)
+    X, y = pad_cell(X, y, dct_config.CROP_SIZE)
 
     props = regionprops(y, cache=False)
     appearances_list = []
@@ -236,11 +116,13 @@ def pipeline_main(data_dir, image_fname, segmask):
 
     model_output = []
 
+    props = props[:5000]
+
     total_num_cells = len(props)
     for prop_idx, prop in enumerate(props):
         curr_cell = prop_idx + 1
         label = prop.label
-        delta = crop_size // 2
+        delta = dct_config.CROP_SIZE // 2
         cbox = get_crop_box(prop.centroid, delta)
         self_mask, neighbor_mask = get_neighbor_masks(
             y, cbox, prop.label
@@ -251,9 +133,9 @@ def pipeline_main(data_dir, image_fname, segmask):
         minr, minc, maxr, maxc = cbox
         raw_patch = X[minr:maxr, minc:maxc, :]  # (H, W, C)
 
-        raw_patch = resize(raw_patch, (rs, rs), preserve_range=True)  # (H, W, C)
-        self_mask = resize(self_mask, (rs, rs), preserve_range=True)  # (H, W)
-        neighbor_mask = resize(neighbor_mask, (rs, rs), preserve_range=True)  # (H, W)
+        raw_patch = resize(raw_patch, (dct_config.PATCH_RESIZE_SIZE, dct_config.PATCH_RESIZE_SIZE), preserve_range=True)  # (H, W, C)
+        self_mask = resize(self_mask, (dct_config.PATCH_RESIZE_SIZE, dct_config.PATCH_RESIZE_SIZE), preserve_range=True)  # (H, W)
+        neighbor_mask = resize(neighbor_mask, (dct_config.PATCH_RESIZE_SIZE, dct_config.PATCH_RESIZE_SIZE), preserve_range=True)  # (H, W)
 
         self_mask = (self_mask > 0.5).astype(np.int32)
         neighbor_mask = (neighbor_mask > 0.5).astype(np.int32)
@@ -271,17 +153,17 @@ def pipeline_main(data_dir, image_fname, segmask):
         num_channels = app.shape[0]
 
         # padding
-        padding_length = MAX_NUM_CHANNELS - num_channels
+        padding_length = dct_config.MAX_NUM_CHANNELS - num_channels
 
         paddings = np.array([[0, padding_length], [0, 0], [0, 0], [0, 0]])
 
-        app_padded = np.pad(
-            app, paddings, mode="constant", constant_values=0
-        )  # (MAX_NUM_CHANNELS, H, W, 3)
+        app_padded = np.pad(app, paddings, mode="constant", constant_values=0)
 
         channel_list_padded = channel_lst + ["None"] * padding_length
 
-        padding_mask = np.zeros((MAX_NUM_CHANNELS, MAX_NUM_CHANNELS), dtype=np.int32)
+        padding_mask = np.zeros(
+            (dct_config.MAX_NUM_CHANNELS, dct_config.MAX_NUM_CHANNELS), dtype=np.int32
+        )
         padding_mask[:num_channels, :num_channels] = 1
 
         padding_mask = np.pad(padding_mask, [[1, 0], [1, 0]])  # for class_token
